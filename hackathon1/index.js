0dbf481b20b48daf8b3f6725e85fee472744e3fc
ronys79 <ronyserrato@gmail.com>
2021-04-22 22:52:30 +0300

    end of nigth

--- hackathon1/index.js
+++ hackathon1/index.js
@@ -13,6 +13,7 @@ class HealthBar {
     }
   
     show(context) {
+      // border around red bar
       context.lineWidth = 4;
       context.strokeStyle = "#333";
       context.fillStyle = this.color;
@@ -32,13 +33,19 @@ class HealthBar {
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+
+// style-size of the outer canvas of health bar
+
 const width = canvas.width = 80;
 const height = canvas.height = 40;
 
-// health bar on top right margin of page
+// use this to position bar on top of page, set to top right margin of page with inner height /2 of canvas
 canvas.style.marginTopRight = window.innerHeight / 2 - height / 2 + "px";
 
+// total life counter
 let health = 100;
+
+// style-size of the actual health bar
 const healthBarWidth = 70;
 const healthBarHeight = 20;
 const x = width / 2 - healthBarWidth / 2;
@@ -51,8 +58,9 @@ const frame = function() {
   healthBar.show(context);
   requestAnimationFrame(frame);
 }
-// health - how much HP lost per wrong answer. set at -33 per wrong answer = 3 lives
+
 canvas.onclick = function() {
+  // health is how much HP lost per wrong answer. set at -33 per wrong answer = 3 lives
   health -= 33;
   healthBar.updateHealth(health);
 };
