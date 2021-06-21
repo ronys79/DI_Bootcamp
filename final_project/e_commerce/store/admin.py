31b836abf01624612e191c7a6163c6cb6f16ed2a
ronys79 <ronyserrato@gmail.com>
2021-06-21 16:14:24 +0300

    start of last week

--- final_project/e_commerce/store/admin.py
+++ final_project/e_commerce/store/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ReviewRating, ProductGallery
+import admin_thumbnails
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
 
 class ProductAdmin(admin.ModelAdmin):
 # populate slug to rule out errors + populated field all lower case with hyphen as space
@@ -8,5 +15,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('category', 'product_name', 'price', 'images', 'stock', 'is_available', 'modified_date')
 # clickable links to get to product specific page or quck view image
     list_display_links = ('category', 'product_name', 'images')
+    inlines = [ProductGalleryInline]
     
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
